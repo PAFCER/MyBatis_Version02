bf6d5e63e31f2b1f429229648e519951d6fb026e
HotWater1991 <15993127580@163.com>
2018-08-05 09:42:19 +0800

    添加了新类TestGit

---
+++ src/test/java/com/zzu/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.zzu.test;
+
+/**
+ * Created by hotwater on 2018/8/5.
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.err.println("测试git的保存新的文件到github上");
+
+    }
+
+}
